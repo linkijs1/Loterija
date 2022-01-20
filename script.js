8f6f28c3ab047b3ecef562495c0ab022f0398bb6
linkijs1 <linkijs69@gmail.com>
2022-01-20 15:04:02 +0200

    otrais

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-alert(1)
+const vardi=['Edgars Laizmanis','Stīvs Gailis', 'Baugis Sukmanis', 'Ben Dover', 'Deez Nuts']
+const balvas=['Ligma','Bakugans', 'Beyblade', 'Pokemons', 'Dators']
+const naudakopa=1000000;//kopeja summa
+let uzvaretajuSkaits=5;
+let rand= Math.random()*5;//nejauši izvēlēsies 5 cilvēkus
+rand= Math.floor(rand);//noapaļo uz leju
+console.log(vardi[rand]);//izveido konsolē
